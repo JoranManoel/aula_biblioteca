8c100b8f6cf2a85b2edf11ae9ad785673eef81e0
JoranManoel <joran.manoel@hotmail.com>
2025-09-29 15:42:38 -0300

    first commit

---
+++ emprestimo.php
@@ -0,0 +1,1 @@
+<h1>Pegar Livro</h1>
---
+++ listarLivros.php
@@ -0,0 +1,30 @@
+<h1>Lista de livros</h1>
+
+<div class="row">
+
+<div class="card m-2" style="width: 18rem;">
+  <img src="https://static.vecteezy.com/ti/vetor-gratis/p1/7442146-ilustracao-simples-com-pilha-de-livros-em-fundo-isolado-vetor.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card m-2" style="width: 18rem;">
+  <img src="https://static.vecteezy.com/ti/vetor-gratis/p1/7442146-ilustracao-simples-com-pilha-de-livros-em-fundo-isolado-vetor.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card m-2" style="width: 18rem;">
+  <img src="https://static.vecteezy.com/ti/vetor-gratis/p1/7442146-ilustracao-simples-com-pilha-de-livros-em-fundo-isolado-vetor.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+</div>
